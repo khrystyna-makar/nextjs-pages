06b535bb70abb870964a2acad3058aab583099af
Khrystyna <khrystyna.makar@gmail.com>
2025-02-10 20:57:44 +0200

    fix

--- app/components/Logo.tsx
+++ app/components/Logo.tsx
@@ -1,3 +1,4 @@
+import { prefix } from "@/utils/prefix";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -6,7 +7,7 @@ const Logo = () => {
     return (
         <>
             <Link href="/" className="flex w-fit gap-x-1">
-                <Image src="/logo.svg" alt="logo" width={64} height={64} />
+                <Image src={`${prefix}/logo.svg`} alt="logo" width={64} height={64} />
                 <p className="w-[76px] text-small1 text-font-primary">Нова 
                 Українська
                 Хвиля</p>
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  env: {
+    NEXT_PUBLIC_BASE_PATH: '/nextjs-pages'
+  }
 };
 
 export default nextConfig;
