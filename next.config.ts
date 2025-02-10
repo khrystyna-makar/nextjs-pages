7ce8a6302775c58a73ac305bb2b7be415f389ace
Khrystyna <64195415+khrystyna-makar@users.noreply.github.com>
2025-02-10 20:09:44 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,7 @@ import type { NextConfig } from "next";
 
 const isProd = process.env.NODE_ENV === 'production';
 const nextConfig: NextConfig = {
-  reactStrictMode: true,
-  images: {
-    unoptimized: true,
-  },
-  assetPrefix: isProd ? '/nextjs-pages/' : '',
-  basePath: isProd ? '/nextjs-pages' : '',
+ 
   output: "export",
 };
 
