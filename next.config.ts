dcdaf1b7213e83f6e551d26cfe548e23b1498781
Khrystyna <64195415+khrystyna-makar@users.noreply.github.com>
2025-02-10 19:52:03 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,6 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
 const nextConfig: NextConfig = {
   assetPrefix: isProd ? '/nextjs-pages/' : '',
   basePath: isProd ? '/nextjs-pages' : '',
